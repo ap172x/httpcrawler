867c97b24e432aa08a15867becc9002019589c9b
ap172x <ap172x@gmail.com>
2023-09-30 14:01:10 +0530

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,30 @@
+const { link } = require('fs')
+const {JSDOM} = require('jsdom')
+
+function getURLsFromHTML(htmlbody, baseURL){
+    const urls = []
+    const dom = new JSDOM(htmlbody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const linkElement of linkElements){
+        if (linkElement.href.slice(0,1) === '/'){
+            try {
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            } catch (error) {
+                console.log(`Error with relative url: ${error.message}`)
+            }
+        } else {
+            try {
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+            } catch (error) {
+                console.log(`Error with absolute url: ${error.message}`)
+            }
+        }
+    }
+    return urls
+}
+
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
@@ -8,5 +35,6 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
